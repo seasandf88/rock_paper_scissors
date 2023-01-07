e69ccf9654312768673fc500a73f8988acc061ce
seasandf88 <engr.f.nassar@gmail.com>
2023-01-07 16:00:30 +0200

    Add UI

--- main.js
+++ main.js
@@ -10,10 +10,78 @@ function getComputerChoice() {
   }
 }
 
-// Game rules and scoring counters
 let win = 0;
 let lose = 0;
 let tie = 0;
+
+//Empty images placeholders
+const pImg = document.querySelector("#pImg");
+const cImg = document.querySelector("#cImg");
+pImg.src = `./images/none.png`;
+cImg.src = `./images/none.png`;
+
+const pScore = document.querySelector("#pScore");
+const cScore = document.querySelector("#cScore");
+const tScore = document.querySelector("#tScore");
+pScore.textContent = "0";
+cScore.textContent = "0";
+tScore.textContent = "0";
+
+const buttons = document.querySelectorAll(".play");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let computerChoice = getComputerChoice();
+    turn(button, computerChoice);
+    pImg.src = `./images/${button.id}2.png`;
+    cImg.src = `./images/${computerChoice}.png`;
+    gsap.from("#pImg", { duration: 0.17, y: "-100%", x: "-20%", opacity: 0 });
+    gsap.from("#cImg", {
+      duration: 0.15,
+      y: "-100%",
+      x: "20%",
+      opacity: 0,
+      delay: 0.02,
+    });
+  });
+});
+
+const overlayDiv = document.querySelector(".overlay");
+const finalMsg = document.querySelector("#finalMsg");
+
+function turn(button, computerChoice) {
+  playRound(button.id, computerChoice);
+  pScore.textContent = win;
+  cScore.textContent = lose;
+  tScore.textContent = tie;
+  if (win == 3) {
+    overlayDiv.classList.add("active");
+    finalMsg.textContent = "You Won!";
+    button.disabled = true;
+  } else if (lose == 3) {
+    overlayDiv.classList.add("active");
+    finalMsg.textContent = "You Lost!";
+    button.disabled = true;
+  }
+}
+
+const finalButton = document.querySelector("#finalButton");
+finalButton.addEventListener("click", () => {
+  overlayDiv.classList.remove("active");
+  reset();
+});
+
+function reset() {
+  pScore.textContent = win = 0;
+  cScore.textContent = lose = 0;
+  tScore.textContent = tie = 0;
+  pImg.src = `./images/none.png`;
+  cImg.src = `./images/none.png`;
+  buttons.forEach((button) => {
+    button.disabled = false;
+  });
+}
+
+// Game rules and scoring counters
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
     tie += 1;
@@ -43,38 +111,4 @@ function playRound(playerChoice, computerChoice) {
       return "You Lose! Rock beats Scissors";
     }
   }
-}
-// Five rounds game
-for (let i = 0; i < 5; i++) {
-  console.log("\n ROUND " + (i + 1));
-  askplayer();
-  // This function check if the player input is correct,
-  // if not, it alert the player and prompt them to enter a value again.
-  function askplayer() {
-    let playerChoice = prompt(
-      "Round: " + (i + 1) + "\nPlease Enter rock, paper or scissors"
-    );
-    playerChoice = playerChoice.toLowerCase();
-    if (
-      playerChoice !== "paper" &&
-      playerChoice !== "rock" &&
-      playerChoice !== "scissors"
-    ) {
-      alert("You did not enter a correct value");
-      askplayer();
-    } else {
-      let computerChoice = getComputerChoice();
-      //console.log(computerChoice);
-      console.log(playRound(playerChoice, computerChoice));
-    }
-  }
-}
-// Game result
-console.log("\nScore: \nWins: " + win + "\nLosses: " + lose + "\nTies: " + tie);
-if (win < lose) {
-  console.log("\n---You Lost!---");
-} else if (win > lose) {
-  console.log("\n---You Won!---");
-} else {
-  console.log("\n---Tie!!!---");
 }
